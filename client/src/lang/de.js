05e1a3f08cf5415ad00d7cc6f82fbc9a2c5748b5
Stephan Bauer <sb@relaxt.at>
2025-11-16 20:43:24 +0000

    Added German translation.

---
+++ client/src/lang/de.js
@@ -0,0 +1,8 @@
+if(typeof(i18n) == 'undefined') {
+    console.error('Class i18n not defined');
+} else {
+    i18n.addDictionary('de', {
+        "Admin.LINKLABEL_PHONE": "Link zu Telefonnummer",
+        "Admin.LINK_PHONE": "Link zu Telefonnummer einfügen"
+    });
+}
